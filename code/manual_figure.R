f833ed60a19a66ec5c511d2877b7abe9e844f8ab
Sophia Wassermann - NOAA <so@sowasser.com>
2026-05-07 17:31:48 -0700

    Added indigenous status and fishing experience to multivariate manual coding plot

--- code/manual_figure.R
+++ code/manual_figure.R
@@ -1,4 +1,3 @@
-library(dplyr)
 library(ggplot2)
 library(here)
 
@@ -7,7 +6,13 @@ theme_set(theme_sleek())
 
 manual <- read.csv(here("data/OHP_table2_manualcoding.csv"))
 
-ggplot(manual, aes(x = Change.Language, y = Emotional.Level)) +
-  geom_jitter(size = 4)
+ggplot(manual, aes(
+  x = Change.Language, 
+  y = Emotional.Level, 
+  color = Fishing.Experience, 
+  shape = Indigenous.Status)) +
+  geom_jitter(size = 2, width = 0.1, height = 0.1)
+
+ggsave(here("output/manual_coding.png"), width = 6, height = 4, units = "in")
 
 
